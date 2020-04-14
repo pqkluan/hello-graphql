ec90554c31ddbaa29be24ac074d68e50cb8fe3e4
pqkluan <luan@eatsyapp.co>
2020-04-14 12:36:06 +0700

    feat: enable cors for hello-next

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const server = new GraphQLServer({
   context: { prisma }
 });
 
+server.express.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://hello-next.pqkluan.now.sh");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.start(() => {
   console.log("Server is running on http://localhost:4000");
 });
